ddadaec76476dc9889fe123cac127d93ec7d8935
Dan <grezindanil@gmail.com>
2026-02-21 12:03:52 +0300

    Add insert, search and free

--- src/bst.h
+++ src/bst.h
@@ -1,14 +1,26 @@
 #pragma once
 
+#inlcude <stdbool.h>
+
 typedef struct Node
 {
     int value;
     struct Node *left;
     struct Node *right;
-    
 }Node;
 
 typedef struct BST
 {
     Node *root;
 }BST;
+
+Node* createNode(int value);
+
+Node* insertNode(Node* node, value);
+void bstInsert(BST* tree, int value);
+
+bool nodeContains(Node* node, int value);
+bool bstContains(BST* tree, int value);
+
+void freeNodes(Node* node);
+void bstFree(BST* tree);
