40c3fb035b63def79f002e333616e5ed6edafb7a
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-02-21 12:56:11 +0300

    Update bst.h

--- src/bst.h
+++ src/bst.h
@@ -1,3 +1,7 @@
+#pragma once
+
+#inlcude <stdbool.h>
+
 typedef struct Node
 {
     int value;
@@ -9,3 +13,37 @@ typedef struct BST
 {
     Node *root;
 }BST;
+
+// TASK A
+Node* createNode(int value);
+
+Node* insertNode(Node* node, value);
+void bstInsert(BST* tree, int value);
+
+bool nodeContains(Node* node, int value);
+bool bstContains(BST* tree, int value);
+
+void freeNodes(Node* node);
+void bstFree(BST* tree);
+
+// TASK B
+void _bstinorder(Node *root);
+void _bstpreorder(Node *root);
+void _bstpostorder(Node *root);
+
+void bstInorder(BST *tree);
+void bstPreorder(BST *tree);
+void bstPostorder(BST *tree);
+
+// TASK C
+int bstHeightRecursive(Node* root);
+int bstHeight(BST* tree);
+
+int bstSizeRecursive(Node* root);
+int bstSize(BST* tree);
+
+//if tree is empty bstMin will return warning and stop the program
+int bstMin(BST* tree);
+
+//if tree is empty bstMax will return warning and stop the program
+int bstMax(BST* tree);
