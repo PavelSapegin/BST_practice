d608f7f14f111a9a378d4abccbdcac917dd73600
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-02-21 12:44:07 +0300

    tree statistic

--- src/bst.h
+++ src/bst.h
@@ -14,6 +14,7 @@ typedef struct BST
     Node *root;
 }BST;
 
+// TASK A
 Node* createNode(int value);
 
 Node* insertNode(Node* node, value);
@@ -24,3 +25,25 @@ bool bstContains(BST* tree, int value);
 
 void freeNodes(Node* node);
 void bstFree(BST* tree);
+
+// TASK B
+void _bstinorder(Node *root);
+void _bstpreorder(Node *root);
+void _bstpostorder(Node *root);
+
+void bstInorder(BST *tree);
+void bstPreorder(BST *tree);
+void bstPostorder(BST *tree);
+
+// TASK C
+int bstHeightRecursive(Node* root);
+int bstHeight(BST* tree);
+
+int bstSizeRecursive(Node* root);
+int bstSize(BST* tree);
+
+//if tree is empty bstMin will return warning and stop the program
+int bstMin(BST* tree);
+
+//if tree is empty bstMax will return warning and stop the program
+int bstMax(BST* tree);
