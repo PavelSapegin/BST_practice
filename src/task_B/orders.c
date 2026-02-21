61781139c47a40379a2696ffe05eec4cba70dac8
PavelSapegin <pawsapegin@yandex.ru>
2026-02-21 11:51:03 +0300

    Add functions to work with BST struct

--- src/task_B/orders.c
+++ src/task_B/orders.c
@@ -1,61 +1,62 @@
 #include <stdio.h>
 #include "bst.h"
 
-void bstInorder(BST *tree)
+void _bstinorder(Node *root)
 {
-    Node *curr = tree;
+    Node *curr = root;
 
     if (curr == NULL)
-    {
         return;
-    }
 
     if (curr->left != NULL)
-    {
-        bstInorder(curr->left);
-    }
+        _bstinorder(curr->left);
 
     printf("%d ", curr->value);
 
     if (curr->right != NULL)
-    {
-        bstInorder(curr->right);
-    }
+        _bstinorder(curr->right);
+}
+void _bstpreorder(Node *root)
+{
+    Node *curr = root;
+    if (curr == NULL)
+        return;
+
+    printf("%d ", curr->value);
+    if (curr->left != NULL)
+        _bstpreorder(curr->left);
+
+    if (curr->right != NULL)
+
+        _bstpreorder(curr->right);
+}
+void _bstpostorder(Node *root)
+{
+    Node *curr = root;
+    if (curr == NULL)
+        return;
+
+    if (curr->left != NULL)
+        _bstpostorder(curr->left);
+
+    if (curr->right != NULL)
+        _bstpostorder(curr->right);
+
+    printf("%d ", curr->value);
+}
+
+void bstInorder(BST *tree)
+{
+    if (tree != NULL)
+        _bstinorder(tree->root);
 }
 void bstPreorder(BST *tree)
 {
-    Node *curr = tree;
-    if (curr == NULL)
-    {
-        return;
-    }
-    printf("%d ", curr->value);
-    if (curr->left != NULL)
-    {
-        bstPreorder(curr->left);
-    }
-    if (curr->right != NULL)
-    {
-        bstPreorder(curr->right);
-    }
+    if (tree != NULL)
+        _bstpreorder(tree->root);
 }
 void bstPostorder(BST *tree)
 {
-    Node *curr = tree;
-    if (curr == NULL)
-    {
-        return;
-    }
-
-    if (curr->left != NULL)
-    {
-        bstPostorder(curr->left);
-    }
-
-    if (curr->right != NULL)
-    {
-        bstPostorder(curr->right);
-    }
-
-    printf("%d ", curr->value);
+    if (tree != NULL)
+        _bstpostorder(tree->root);
 }
