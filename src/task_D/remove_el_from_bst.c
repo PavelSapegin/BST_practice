70fe8b07bc96b274168058c8307a451ecb9fdf7a
RyabukhinaMariya <1411mashakrasnodar@gmail.com>
2026-02-27 00:21:52 +0300

    Add bstDelete

--- src/task_D/remove_el_from_bst.c
+++ src/task_D/remove_el_from_bst.c
@@ -1,58 +1,60 @@
 #include "../bst.h"
 
-voidbstDelete(BST* tree, int value) {
-    if (tree == NULL || tree->root == NULL) return;
-    
-    bool deleted = false;
-    tree->root = deleteNode(tree->root, value, &deleted);
-}
-
-Node* find_min(Node* root){
-    if (root == NULL){
+Node *find_min(Node *root)
+{
+    if (root == NULL) {
         return NULL;
     }
 
-    Node* current = root;
-    while (current->left != NULL){
+    Node *current = root;
+    while (current->left != NULL) {
         current = current->left;
     }
     return current;
 }
 
-Node* deleteNode(Node* root, int val, bool* deleted){
-    if (root == NULL){
+Node *deleteNode(Node *root, int val, bool *deleted)
+{
+    if (root == NULL) {
         return NULL;
     }
 
-    if (val < root->value){
+    if (val < root->value) {
         root->left = deleteNode(root->left, val, deleted);
-    }
-    else if (val > root->value){
+    } else if (val > root->value) {
         root->right = deleteNode(root->right, val, deleted);
-    }
-    else{
+    } else {
         *deleted = true;
 
-        //case no left
-        if (root->left == NULL){
-            Node* temp = root->right;
+        // case no left
+        if (root->left == NULL) {
+            Node *temp = root->right;
             free(root);
             return temp;
         }
-        //case no right
-        else if (root->right == NULL){
-            Node* temp = root->left;
+        // case no right
+        else if (root->right == NULL) {
+            Node *temp = root->left;
             free(root);
             return temp;
         }
-        
-        //case 2 child 
-        Node* min_right = find_min(root->right);
+
+        // case 2 child
+        Node *min_right = find_min(root->right);
         root->value = min_right->value;
-        //to avoid overwriting make new deleted flag
+        // to avoid overwriting make new deleted flag
         bool temp_deleted = false;
         root->right = deleteNode(root->right, min_right->value, &temp_deleted);
     }
     return root;
 }
 
+void bstDelete(BST *tree, int value)
+{
+    if (tree == NULL || tree->root == NULL) {
+        return;
+    }
+
+    bool deleted = false;
+    tree->root = deleteNode(tree->root, value, &deleted);
+}
