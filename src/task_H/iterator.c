0818a746baae25be6e32ab4a0cd965f184ea8e34
PavelSapegin <pawsapegin@yandex.ru>
2026-02-28 14:50:22 +0300

    Formatting code

--- src/task_H/iterator.c
+++ src/task_H/iterator.c
@@ -1,16 +1,15 @@
 #include "../bst.h"
 
-
 Node* pop(Iterator* it)
 {
-    Node *result = it->top->node;
-    stack *temp = it->top->next;
+    Node* result = it->top->node;
+    stack* temp = it->top->next;
     free(it->top);
     it->top = temp;
     return result;
 }
 
-void push(Iterator *it, Node* new)
+void push(Iterator* it, Node* new)
 {
     stack* new_top = malloc(sizeof(stack));
     new_top->node = new;
@@ -18,55 +17,47 @@ void push(Iterator *it, Node* new)
     it->top = new_top;
 }
 
-
-Iterator* iteratorInit(BST *tree)
+Iterator* iteratorInit(BST* tree)
 {
-    stack *top = NULL;
+    stack* top = NULL;
     Iterator* it = malloc(sizeof(Iterator));
     it->top = top;
     Node* curr = tree->root;
-    while (curr != NULL)
-    {
-        push(it,curr);
+    while (curr != NULL) {
+        push(it, curr);
         curr = curr->left;
     }
 
     return it;
 }
 
-bool iteratorHasNext(Iterator *it)
+bool iteratorHasNext(Iterator* it)
 {
     return (it->top != NULL);
 }
 
-int iteratorNext(Iterator *it)
+int iteratorNext(Iterator* it)
 {
-    if (iteratorHasNext(it))
-    {
-        Node *resultNode = pop(it);
-        Node *curr = resultNode->right;
-        while (curr != NULL)
-        {
-            push(it,curr);
+    if (iteratorHasNext(it)) {
+        Node* resultNode = pop(it);
+        Node* curr = resultNode->right;
+        while (curr != NULL) {
+            push(it, curr);
             curr = curr->left;
         }
 
         return resultNode->value;
-    }
-    else
+    } else
         return -42;
-    
 }
 
-
-void iteratorFree(Iterator *it)
+void iteratorFree(Iterator* it)
 {
     if (it == NULL)
         return;
 
-    while (iteratorHasNext(it))
-    {
-        Node *curr = pop(it);
+    while (iteratorHasNext(it)) {
+        Node* curr = pop(it);
     }
 
     free(it);
